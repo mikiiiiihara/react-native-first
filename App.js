138ed1fb278f2574da94ddb833ffe6fe5fbe927c
mi-ihara <kimity0824@icloud.com>
2022-10-18 03:25:19 +0900

    reactNativeのサンプル作成

--- App.js
+++ App.js
@@ -1,20 +1,71 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { StyleSheet, View, FlatList } from 'react-native';
+import Header from './components/Header';
+import Footer from './components/Footer';
+import ListUser from './components/ListUser';
+import AddUser from './components/AddUser';
+import 'react-native-get-random-values';
+import { v4 as uuidv4 } from 'uuid';
 
 export default function App() {
+  const [users, setUsers] = useState([]);
+
+  const addUser = (name) => {
+    setUsers((prevUsers) => {
+      return [{ id: uuidv4(), name }, ...prevUsers];
+    });
+  };
+  const deleteUser = (id) => {
+    setUsers((prevUsers) => {
+      return prevUsers.filter((prevUser) => prevUser.id !== id);
+    });
+  };
+
+  useEffect(() => {
+    const getUser = async () => {
+      const res = await fetch('https://jsonplaceholder.typicode.com/users');
+      const users = await res.json();
+      setUsers(users);
+    };
+    getUser();
+  }, []);
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <Header title="ユーザ一覧" />
+      <FlatList
+        data={users}
+        renderItem={({ item }) => (
+          <ListUser item={item} deleteUser={deleteUser} />
+        )}
+        keyExtractor={(item) => item.id}
+        ItemSeparatorComponent={() => (
+          <View
+            style={{
+              backgroundColor: 'lightgray',
+              height: 1,
+            }}
+          ></View>
+        )}
+      />
+      <Footer />
     </View>
   );
 }
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+  },
+  item: {
+    flexDirection: 'row',
     alignItems: 'center',
-    justifyContent: 'center',
+    padding: 10,
+    backgroundColor: '#88cb7f',
+  },
+  avatar: {
+    height: 50,
+    width: 50,
+    borderRadius: 30,
+    marginRight: 8,
   },
 });
